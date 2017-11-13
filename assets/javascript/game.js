a91cc93ecac37c6ef6022a7b76da44a8f3df73b6
Jennifer Stokes <jenniferstokes@Jennifers-MacBook-Pro.local>
2017-11-13 11:30:33 -0800

    random number no button

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,23 @@
 //generates random number when button is pushed
 
+
+
 $(document).ready(function(){
-	$("#random-button").on("click", function(){
-		var randomNumber = Math.floor(Math.random() * 100) +1;
+	var randomNumber = Math.floor(Math.random() * 100) +1;
 		$("#random-number").html(randomNumber);
-		
 	})
 
 
 
+
 	
-	});
+
+
+	
+
+	
+	
+
+
+	
 
